ba55cd443d19eee15bae6b402f4a1c97cdccfb90
Dan Mazz <daniel@wizr.com>
2016-04-15 12:17:54 -0700

    logins

--- db/users.js
+++ db/users.js
@@ -1,8 +1,13 @@
 var records = [
     { id: 1, username: 'panasonic', password: 'na', displayName: 'david', emails: [ { value: 'david@wizr.com' } ] },
     { id: 2, username: 'wizr', password: 'na', displayName: 'donny', emails: [ { value: 'donny@wizr.com' } ] },
-    { id: 2, username: 'sanjeev', password: 'na', displayName: 'donny', emails: [ { value: 'sanjeev@wizr.com' } ] },
-    { id: 2, username: 'nader', password: 'na', displayName: 'donny', emails: [ { value: 'sanjeev@wizr.com' } ] }
+    { id: 3, username: 'panasonic', password: 'na', displayName: 'david', emails: [ { value: 'panasonic@wizr.com' } ] },
+    { id: 4, username: 'milestone', password: 'na', displayName: 'david', emails: [ { value: 'milestone@wizr.com' } ] },
+    { id: 5, username: 'eagleeye', password: 'na', displayName: 'david', emails: [ { value: 'eagleeye@wizr.com' } ] },
+    { id: 6, username: 'adt', password: 'na', displayName: 'david', emails: [ { value: 'adt@wizr.com' } ] },
+    { id: 7, username: 'netgear', password: 'na', displayName: 'david', emails: [ { value: 'netgear@wizr.com' } ] },
+    { id: 8, username: 'pelco', password: 'na', displayName: 'david', emails: [ { value: 'pelco@wizr.com' } ] },  
+    { id: 9, username: 'comcast', password: 'na', displayName: 'david', emails: [ { value: 'comcast@wizr.com' } ] }    
 ];
 
 exports.findById = function(id, cb) {
